b1239de431ef838f4f02c00e59817577a8acdd99
hdish <115708623+hdish@users.noreply.github.com>
2026-02-02 20:50:24 +0800

    Delete machine/day01/day02 directory

--- machine/day01/day02/03-KNN_lris.py
+++
@@ -1,9 +0,0 @@
-from sklearn.datasets import load_iris
-
-# 导入数据
-iris_data = load_iris()
-
-print(f'{iris_data.data[:5]}')
-print(f'{iris_data.target}')
-print(f'{iris_data.target_names}')
-print(f'{iris_data.feature_names}')
